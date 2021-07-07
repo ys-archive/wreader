7d0fcb066685bad9adf37332700d458e0d67c20d
yoonsang <highp0912@pm.me>
2021-07-08 01:56:56 +0900

    Add Header Left/Right in Home Screen

--- src/navigators/level2/MainStack.jsx
+++ src/navigators/level2/MainStack.jsx
@@ -2,13 +2,38 @@ import React from 'react';
 import * as ScreenNames from '../ScreenNames';
 import { createStackNavigator } from '@react-navigation/stack';
 import Main from '../../screens/Main';
+import { AntDesign } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 
 const MainStack = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name={ScreenNames.Main} component={Main} />
+    <Stack.Navigator>
+      <Stack.Screen
+        name={ScreenNames.Main}
+        component={Main}
+        options={({ navigation, route }) => ({
+          title: null,
+          headerLeft: () => (
+            <AntDesign
+              name="home"
+              size={24}
+              color="black"
+              style={{ marginLeft: 24 }}
+              onPress={() => navigation.navigate(ScreenNames.MainStack)}
+            />
+          ),
+          headerRight: () => (
+            <AntDesign
+              name="user"
+              size={24}
+              color="black"
+              style={{ marginRight: 24 }}
+              onPress={() => navigation.navigate(ScreenNames.MyProfileStack)}
+            />
+          ),
+        })}
+      />
     </Stack.Navigator>
   );
 };
