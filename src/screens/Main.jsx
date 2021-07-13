1271ee5ae038a757b80e6f997871fd58833f78b8
yoon sang Lee <highp0912@pm.me>
2021-07-13 14:43:50 +0900

    WIP Swiper

--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -6,7 +6,6 @@ import React, { useRef, useState } from 'react';
 import {
   Animated,
   PanResponder,
-  Dimensions,
   LayoutAnimation,
   Platform,
   View,
@@ -18,11 +17,12 @@ import EventModal from '#components/modals/EventModal';
 import Novel from '../data/novel/novel';
 import { PanGestureHandler } from 'react-native-gesture-handler';
 
+import Swiper from './swiper/Swiper';
+
 import NovelCard from './NovelCard';
 
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
-const SWIPE_THRESHOLD = 0.4 * SCREEN_WIDTH;
-const SWIPE_OUT_DURATION = 250;
+// const SWIPE_THRESHOLD = 0.4 * SCREEN_WIDTH;
+// const SWIPE_OUT_DURATION = 250;
 
 // class Novel {
 //   state = {
@@ -66,79 +66,79 @@ const SWIPE_OUT_DURATION = 250;
 // }
 
 const Main = () => {
-  const position = useRef(new Animated.ValueXY()).current;
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder() {
-        return true;
-      },
+  // const position = useRef(new Animated.ValueXY()).current;
+  // const panResponder = useRef(
+  //   PanResponder.create({
+  //     onStartShouldSetPanResponder() {
+  //       return true;
+  //     },
 
-      onPanResponderMove(e, state) {
-        const { dx, dy } = state;
-        position.setValue({ x: dx, y: dy });
-      },
+  //     onPanResponderMove(e, state) {
+  //       const { dx, dy } = state;
+  //       position.setValue({ x: dx, y: dy });
+  //     },
 
-      onPanResponderRelease(e, state) {
-        const { dx, dy } = state;
+  //     onPanResponderRelease(e, state) {
+  //       const { dx, dy } = state;
 
-        if (dx > SWIPE_THRESHOLD) {
-          forceSwipeHorizontally('right');
-        } else if (dx < -SWIPE_THRESHOLD) {
-          forceSwipeHorizontally('left');
-        } else if (dy > SWIPE_THRESHOLD) {
-          forceSwipeVertically('up');
-        } else if (dy < -SWIPE_THRESHOLD) {
-          forceSwipeVertically('down');
-        } else {
-          resetPosition();
-        }
-      },
-    }),
-  ).current;
+  //       if (dx > SWIPE_THRESHOLD) {
+  //         forceSwipeHorizontally('right');
+  //       } else if (dx < -SWIPE_THRESHOLD) {
+  //         forceSwipeHorizontally('left');
+  //       } else if (dy > SWIPE_THRESHOLD) {
+  //         forceSwipeVertically('up');
+  //       } else if (dy < -SWIPE_THRESHOLD) {
+  //         forceSwipeVertically('down');
+  //       } else {
+  //         resetPosition();
+  //       }
+  //     },
+  //   }),
+  // ).current;
 
-  const forceSwipeVertically = dir => {
-    const isUp = dir === 'up';
-    const swipeAmount = SCREEN_HEIGHT * 1.5 * (isUp ? -1 : 1);
-    Animated.timing(position, {
-      toValue: { x: 0, y: swipeAmount },
-      duration: SWIPE_OUT_DURATION,
-      useNativeDriver: false,
-    }).start();
-  };
+  // const forceSwipeVertically = dir => {
+  //   const isUp = dir === 'up';
+  //   const swipeAmount = SCREEN_HEIGHT * 1.5 * (isUp ? -1 : 1);
+  //   Animated.timing(position, {
+  //     toValue: { x: 0, y: swipeAmount },
+  //     duration: SWIPE_OUT_DURATION,
+  //     useNativeDriver: false,
+  //   }).start();
+  // };
 
-  const forceSwipeHorizontally = dir => {
-    const isLeft = dir === 'left';
-    const swipeAmount = SCREEN_HEIGHT * 1.5 * (isLeft ? -1 : 1);
-    Animated.timing(position, {
-      toValue: { x: swipeAmount, y: 0 },
-      duration: SWIPE_OUT_DURATION,
-      useNativeDriver: false,
-    }).start();
-  };
+  // const forceSwipeHorizontally = dir => {
+  //   const isLeft = dir === 'left';
+  //   const swipeAmount = SCREEN_HEIGHT * 1.5 * (isLeft ? -1 : 1);
+  //   Animated.timing(position, {
+  //     toValue: { x: swipeAmount, y: 0 },
+  //     duration: SWIPE_OUT_DURATION,
+  //     useNativeDriver: false,
+  //   }).start();
+  // };
 
-  const resetPosition = () => {
-    Animated.spring(position, {
-      toValue: { x: 0, y: 0 },
-      useNativeDriver: false,
-    }).start();
-  };
+  // const resetPosition = () => {
+  //   Animated.spring(position, {
+  //     toValue: { x: 0, y: 0 },
+  //     useNativeDriver: false,
+  //   }).start();
+  // };
 
-  const getCardStyle = () => {
-    // const rotate = position.x.interpolate({
-    //   inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
-    //   outputRange: ['-120deg', '0deg', '120deg'],
-    // });
+  // const getCardStyle = () => {
+  //   // const rotate = position.x.interpolate({
+  //   //   inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+  //   //   outputRange: ['-120deg', '0deg', '120deg'],
+  //   // });
 
-    // const translate = position.x.interpolate({
-    //   inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
-    //   outputRange: [],
-    // });
+  //   // const translate = position.x.interpolate({
+  //   //   inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+  //   //   outputRange: [],
+  //   // });
 
-    return {
-      ...position.getLayout(),
-      // transform: [{ rotate }],
-    };
-  };
+  //   return {
+  //     ...position.getLayout(),
+  //     // transform: [{ rotate }],
+  //   };
+  // };
 
   const [Novels, setNovels] = useState([
     { id: 0, title: 'genre1' },
@@ -151,17 +151,28 @@ const Main = () => {
   if (Novels.length <= 0) return null;
 
   return (
+    // <View style={s.root}>
+    //   <Animated.View
+    //     style={[getCardStyle(), { position: 'absolute', width: SCREEN_WIDTH }]}
+    //     {...panResponder.panHandlers}
+    //   >
+    //     <EventModal />
+    //     {Novels.map(novel => {
+    //       const { id, title } = novel;
+    //       return <NovelCard key={id} title={title} />;
+    //     })}
+    //   </Animated.View>
+    // </View>
     <View style={s.root}>
-      <Animated.View
-        style={[getCardStyle(), { position: 'absolute', width: SCREEN_WIDTH }]}
-        {...panResponder.panHandlers}
-      >
-        <EventModal />
-        {Novels.map(novel => {
-          const { id, title } = novel;
-          return <NovelCard key={id} title={title} />;
-        })}
-      </Animated.View>
+      <EventModal />
+      <Swiper direction="left" />
+      {/* <Swiper direction="right"/>
+      <Swiper direction="up"/>
+      <Swiper direction="left"/> */}
+      {Novels.map(novel => {
+        const { id, title } = novel;
+        return <NovelCard key={id} title={title} />;
+      })}
     </View>
   );
 };
