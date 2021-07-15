068cf549452ba6cbc2275c51978a28177087f25a
yoon sang Lee <highp0912@pm.me>
2021-07-15 10:06:04 +0900

    Add return for await func

--- src/constants/fonts.js
+++ src/constants/fonts.js
@@ -4,7 +4,7 @@ export const regular = 'open-sans';
 export const bold = 'open-sans-bold';
 
 export const loadFontsAsync = async () => {
-  await Font.loadAsync({
+  return await Font.loadAsync({
     'open-sans': require('../../assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('../../assets/fonts/OpenSans-Bold.ttf'),
   });
