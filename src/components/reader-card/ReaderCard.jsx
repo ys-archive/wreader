638230809561b7a52346eab375c982d34ad8bcf2
yoon sang Lee <highp0912@pm.me>
2021-07-21 00:52:17 +0900

    TODO: Reader Card 배치 추가

--- src/components/reader-card/ReaderCard.jsx
+++ src/components/reader-card/ReaderCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, Dimensions } from 'react-native';
 import { Text, StyleSheet } from '#components';
+import { FontAwesome } from 'react-native-vector-icons';
 
 // TODO: change image uri -> fetched (received from the api)
 const image = require('../../../assets/images/dummy-image.jpg');
@@ -11,17 +12,39 @@ const ReaderCard = ({ path: uri, title, content }) => {
   // console.log(uri);
   return (
     <View style={s.root}>
+      <View style={s.cardTopView}>
+        <Text>챕터 1</Text>
+        <FontAwesome name="sort-amount-desc" size={24} color="black" />
+      </View>
       <View style={s.cardImageView}>
         <Image
           source={image}
           resizeMode="cover"
-          style={{ width: width * 0.8, height: height * 0.8 }}
+          style={{ width: width * 0.8, height: height * 0.6 }}
         />
       </View>
       <Text style={s.cardOverlay} textStyle={s.cardOverlayText}>
         {content ?? ''}
       </Text>
-      <Text isBold>{title}</Text>
+      <View style={{ flexDirection: 'row' }}>
+        {/* Profile */}
+        <View style={{ flexDirection: 'row'}}> 
+          {/* TODO: 프로필 사진 */}
+          {/* TODO: 유저 이름 */}
+        </View>
+
+        {/* Comments */}
+        <View style={{ flexDirection: 'row'}}> 
+          {/* TODO: 코멘트 아이콘 */}
+          {/* TODO: 코멘트 카운트 */}
+        </View>
+
+        {/* Likes */}
+        <View style={{ flexDirection: 'row'}}> 
+          {/* TODO: 좋아요 아이콘 */}
+          {/* TODO: 좋아요 개수 */}
+        </View>
+      </View>
     </View>
   );
 };
@@ -32,7 +55,15 @@ const s = StyleSheet.create({
   root: {
     width,
     height,
-    alignItems: 'center',
+    paddingHorizontal: 35,
+    // alignItems: 'center',
+  },
+  cardTopView: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignSelf: 'flex-start',
+    marginTop: 40,
+    marginBottom: 10,
   },
   cardImageView: {
     borderWidth: 1,
@@ -40,9 +71,9 @@ const s = StyleSheet.create({
   },
   cardOverlay: {
     position: 'absolute',
-    left: 0,
-    top: '10%',
+    left: '15%',
+    top: '25%',
     padding: 55,
-    fontSize: 25,
+    fontSize: 23,
   },
 });
