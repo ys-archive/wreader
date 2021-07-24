b4dbd9663cfe142b0defae2faa6aa5cd7017082a
yoon sang Lee <highp0912@pm.me>
2021-07-24 16:19:02 +0900

    기종별 이미지 선택 로직 추가 (mdpi ~ xxxhdpi)

--- src/utils/image.js
+++ src/utils/image.js
@@ -5,37 +5,46 @@ import {
   heightPercentageToDP as dp,
 } from 'react-native-responsive-screen';
 
-export const getImagePathByScreenResolution = path => {
-  if (path === '') {
-    throw new Error('경로가 비어있으면 안 됩니다!');
+export const getImagePathByScreenResolution = imageFileName => {
+  if (imageFileName === '') {
+    throw new Error('이미지 파일 이름이 비어있으면 안 됩니다!');
   }
 
   const ratio = PixelRatio.get();
+  let imageRoot = undefined;
 
   // android - mdpi
   // ios - x
   if (ratio < 1) {
+    imageRoot = '!images/mdpi';
   }
 
   // android - hdpi
   // ios - x
   if (ratio <= 1 && ratio < 2) {
+    imageRoot = '!images/hdpi';
   }
 
   // android - xhdpi
   // ios - iPhone se, 6s, 7, 8, xr, 11
   if (ratio <= 2 && ratio < 3) {
+    imageRoot = '!images/xhdpi';
   }
 
   // android - xxhdpi, pixel, pixel2
   // ios - 6s+, 7+, 8+, x, xs, xs max, 11 pro, 11 pro max
   if (ratio <= 3 && ratio < 3.5) {
+    imageRoot = '!images/xxhdpi';
   }
 
   // anroid - xxxhdpi, pixel xl, pixel 2 xl, nexus 6
   // ios - x
   if (ratio >= 3.5) {
+    imageRoot = '!images/xxxhdpi';
   }
+
+  const actualPath = `${imageRoot}${imageFileName}`;
+  return actualPath;
 };
 
 export const getImageExtension = uri => {
